0bd1c9f5903f040c93f5f3d7f01aadf77166e3cd
Ankit <155639266+gojo22k@users.noreply.github.com>
2025-01-26 20:43:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,20 +13,14 @@ MESSAGE = '♦️ DONE UPDATING ♦️'
 GIT_TOKEN = os.getenv('GIT_TOKEN', '')
 
 # API keys for different platforms
-MIXDROP_API_KEY = 'gAR2UJ0JE2RKlhJJCqE'
+RPMSHARE_API_KEY = 'b57f6ad44bf1fb528c57ea90'
 FILEMOON_API_KEY = '42605q5ytvlhmu9eris67'
-VIDHIDE_API_KEY = '27261mmymf1etcdwfdvr3'
-STREAMWISH_API_KEY = '10801lny3tlwanfupzu4m'
-DOODSTREAM_API_KEY = '355892lri7tbejk8bpq7vt'
-HYDRAX_API_KEY = 'fb4feb8eea990f7ff7d922111cfc25d8'
+
 
 # URLs for fetching data
 PLATFORMS = {
     'Filemoon': f'https://filemoon-api.vercel.app/api/filemoon?key={FILEMOON_API_KEY}&fld_id=0',
-    'MixDrop': f'https://api.mixdrop.ag/folderlist?email=mohapatraankit22@gmail.com&key={MIXDROP_API_KEY}&id=0',
-    'VidHide': f'https://vidhideapi.com/api/folder/list?key={VIDHIDE_API_KEY}&fld_id=0',
-    'StreamWish': f'https://api.streamwish.com/api/folder/list?key={STREAMWISH_API_KEY}&fld_id=0',
-    'DoodStream': f'https://doodapi.com/api/folder/list?key={DOODSTREAM_API_KEY}'
+    'RpmShare': f'https://rpmshare.com/api/folder/list?key={RPMSHARE_API_KEY}&fld_id=0'
 }
 
 API_URLS = {
